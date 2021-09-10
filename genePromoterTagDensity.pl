d978de14da87ee9ce0c5b60fb8f3478feb5e8429
Dhirendra Kumar <dhirendra8kumar@gmail.com>
2021-09-10 11:09:44 -0400

    Add files via upload

--- genePromoterTagDensity.pl
+++ genePromoterTagDensity.pl
@@ -8,15 +8,15 @@
 $numArgs = $#ARGV + 1;
 if($numArgs != 16) 
 { 
-  print "\nUSAGE: genePromoterTagDensity.pl -i <file> -b <file> -s <file> -n <1/0> -d <1/0> -w <number> -o <file>\n"; 
-  print "\t-i\tFile containing list of genes information\n";
-  print "\t-b\t*.bed file 1 with tags in BED format\n";
-  print "\t-s\t*File containing chromsome lengths in UCSC format (https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/mm9.chrom.sizes)\n";
-  print "\t-n\t1 for normalization by number of tags, 0 otherwise\n";
-  print "\t-F\tAverage fragment length for tag shifting, e.g., 160, 200, 300 \n";
+  print "\nUSAGE: genePromoterTagDensity.pl -i <input-file> -b <input-file> -s <input-file> -n <1/0> -F <int> -r <1/0> -w <int> -o <output-file>\n\n"; 
+  print "\t-i\tinput file fontaining list of genes in UCSC format\n";
+  print "\t-b\tinput file containing ChIP-Seq reads/tags in BED format\n";
+  print "\t-s\tinput file containing chromsome lengths in UCSC format (https://hgdownload.soe.ucsc.edu/goldenPath/mm9/bigZips/mm9.chrom.sizes)\n";
+  print "\t-n\t1 for normalization by total number of reads in BED file, 0 otherwise\n";
+  print "\t-F\tAverage ChIP-Seq fragment length for read shifting, e.g., 160, 200, 300 \n";
   print "\t-r\t1 (-1/0) if same (opposite, both resp.) strand reads need to be considered; e.g., Gro-Seq data\n";
-  print "\t-w\t number of base pairs to be considered on both sides of the TSS; e.g., 500\n";
-  print "\t-o\t*name of the output file\n";
+  print "\t-w\tnumber of base pairs up- and down-stream of TSS, for promoter definition ; e.g., 500\n";
+  print "\t-o\toutput file into which results will be stored\n";
   die "\n";
 }
 
